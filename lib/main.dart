c44039ee49421656156108ad58344d4223b1c993
Joel Fintan <joelfintan@gmail.com>
2020-04-25 14:54:59 +0530

    Added options

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello there'),);
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('Question 1'),
+            RaisedButton(
+              child: Text('Option1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Option1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Option1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Option1'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
